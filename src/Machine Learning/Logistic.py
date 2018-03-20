69f7e8353be3621c7fc8841812b934c4ac32d907
Sergen <sergenispir54@gmail.com>
2018-03-20 20:40:52 +0300

    ml
    update

--- src/Machine Learning/Logistic.py
+++ src/Machine Learning/Logistic.py
@@ -6,17 +6,21 @@ from sklearn import linear_model
 c = [0,0.5,1,1.5,2,2.5,3,3.5,4]
 x = data[:,:-1] ## take all data expect graduation gpa column
 y = x[:,-1] ## class label
+x = np.delete(x,-1,1) ## remove class column from input data
 
 ## change y label with class value (eg. if y[0] = 3.5 then it become 7th class)
 for i in range(y.size):
 	index = c.index(y[i])
 	y[i] = index
 
-logistic = linear_model.LogisticRegression() ## create a logistic regression model
-
 #define model parameters
-error_rate = 0.001 ##this value defines tolerance for stopping criteria.
+tolerance = 0.0001 ##this value defines tolerance for stopping criteria.
 iteration = 1000 ## number of max iteration
-solver = "lbfgs" ##optimization algorithm
+solv = "lbfgs" ##optimization algorithm
 
+logistic = linear_model.LogisticRegression(max_iter = iteration, solver = solv, tol=tolerance) ## create a logistic regression model
 
+logistic.fit(x,y)
+
+print(logistic.get_params())
+print("Accuracy: ", logistic.score(x,y))      
