c9ebec54a50d1f5a60d482a5105cd868c75fea74
Sergen <sergenispir54@gmail.com>
2018-03-23 17:10:05 +0300

    update

--- src/Machine Learning/Sergen/Tables.py
+++ src/Machine Learning/Sergen/Tables.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import Imputer
 
 ## Preprocessing: Convert NaN values to numeric values
 def convert_nan_to_numeric(data):
-    data = table.values ## convert pandas DataFrame to Numpy ndarray
+    data = data.values ## convert pandas DataFrame to Numpy ndarray
     imp = Imputer(missing_values='NaN', strategy='most_frequent', axis=0) 
     imp.fit(data) 
     data = imp.transform(data) ## data holds necesserry numpy table
@@ -25,9 +25,11 @@ grades['CourseCode'] = grades['CourseCode'].apply(str)+ grades['CourseNum'].appl
 grades = grades.drop(columns=["CourseNum"])
 courseTable = grades.pivot_table(values='Grade', index='StudID', columns='CourseCode', aggfunc='first')
 courseTable = courseTable.replace([None,"AA","BA","BB","CB","CC","DC","DD","FD","FF"], [np.nan,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.5,0])
-
 dropoutTable = courseTable
 dropoutTable['Dropout'] = students["Leave"]
+
+courseTable = convert_nan_to_numeric(courseTable)
+
 
 
 
